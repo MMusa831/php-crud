a8e1744dff746a8338c5f5b604a4665dcd50f981
Mohamed Musa <mohamed.musa831@gmail.com>
2022-10-30 12:48:03 +0100

    add component

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <!-- Font awesome -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.0/css/fontawesome.min.css" />
+    <title>PHP Crud</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-light justify-content-center fs-3 mb-5" style="background-color: #00ff5573">
+        Php DRUD Application
+    </nav>
--- index.php
+++ index.php
@@ -1,71 +1,54 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+include "includes/header.php";
+?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <!-- Font awesome -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.0/css/fontawesome.min.css" />
-    <title>PHP Crud</title>
-</head>
-
-<body>
-    <nav class="navbar navbar-light justify-content-center fs-3 mb-5" style="background-color: #00ff5573">
-        Php DRUD Application
-    </nav>
-
-    <div class="container">
-        <?php
-        include "db_conn.php";
-        if (isset($_GET['msg'])) {
-            $msg = $_GET['msg'];
-            echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">' . $msg . '
+<div class="container">
+    <?php
+    include "db_conn.php";
+    if (isset($_GET['msg'])) {
+        $msg = $_GET['msg'];
+        echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">' . $msg . '
                  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                  </div>';
-        }
-        ?>
-        <a href="add_new.php" class="btn btn-dark mb-3">Add new</a>
+    }
+    ?>
+    <a href="add_new.php" class="btn btn-dark mb-3">Add new</a>
 
-        <table class="table table-hover text-center">
-            <thead class="table-dark">
+    <table class="table table-hover text-center">
+        <thead class="table-dark">
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Gender</th>
+                <th scope="col">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $sql = "SELECT * FROM `students`";
+            $result = mysqli_query($conn, $sql);
+
+            while ($row = mysqli_fetch_assoc($result)) {
+            ?>
                 <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">First Name</th>
-                    <th scope="col">Last Name</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Gender</th>
-                    <th scope="col">Action</th>
+                    <td scope="row"><?php echo $row['id']; ?></td>
+                    <td><?php echo $row['first_name']; ?></td>
+                    <td><?php echo $row['last_name']; ?></td>
+                    <td><?php echo $row['email']; ?></td>
+                    <td><?php echo $row['gender']; ?></td>
+                    <td>
+                        <a href="edit.php?id=<?php echo $row['id'] ?>" class="btn btn-success">Edit</a>
+                        <a href="remove.php?id=<?php echo $row['id'] ?>" class="btn btn-danger">Remove</a>
+                    </td>
                 </tr>
-            </thead>
-            <tbody>
-                <?php
-                $sql = "SELECT * FROM `students`";
-                $result = mysqli_query($conn, $sql);
-
-                while ($row = mysqli_fetch_assoc($result)) {
-                ?>
-                    <tr>
-                        <td scope="row"><?php echo $row['id']; ?></td>
-                        <td><?php echo $row['first_name']; ?></td>
-                        <td><?php echo $row['last_name']; ?></td>
-                        <td><?php echo $row['email']; ?></td>
-                        <td><?php echo $row['gender']; ?></td>
-                        <td>
-                            <a href="edit.php?id=<?php echo $row['id'] ?>" class="btn btn-success">Edit</a>
-                            <a href="remove.php?id=<?php echo $row['id'] ?>" class="btn btn-danger">Remove</a>
-                        </td>
-                    </tr>
-                <?php
-                }
-                ?>
-            </tbody>
-        </table>
-    </div>
-    <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-</body>
-
-</html>
+            <?php
+            }
+            ?>
+        </tbody>
+    </table>
+</div>
+<?php
+include "includes/footer.php";
+?>
